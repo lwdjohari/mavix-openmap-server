28cab6454c904e09b0a54b4d688e60729b4dd012
lwdjohari <linggawasistha.djohari@outlook.com>
2024-03-01 19:53:45 +0800

    Add library mavix-storage for handling map data storage layer operations

---
+++ deps/mavix-storage/src/mavix/v1/storage/helper.cc
@@ -0,0 +1,16 @@
+#include "mavix/v1/storage/helper.h"
+
+#include "helper.h"
+
+namespace mavix {
+namespace v1 {
+
+namespace storage {
+Helper::Helper() {}
+
+Helper::~Helper() {}
+}  // namespace storage
+
+}  // namespace v1
+
+}  // namespace mavix
