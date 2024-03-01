91f39316622aae1573e599d55e27e70e68cb1fb9
lwdjohari <linggawasistha.djohari@outlook.com>
2024-03-01 19:55:10 +0800

    Add library mavix-tiles for handling map tiles operations layer

---
+++ deps/mavix-tiles/src/mavix/v1/tiles/helper.cc
@@ -0,0 +1,16 @@
+#include "mavix/v1/tiles/helper.h"
+
+#include "helper.h"
+
+namespace mavix {
+namespace v1 {
+
+namespace tiles {
+Helper::Helper() {}
+
+Helper::~Helper() {}
+}  // namespace tiles
+
+}  // namespace v1
+
+}  // namespace mavix
---
+++ deps/mavix-tiles/src/mavix/v1/tiles/helper.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace mavix {
+namespace v1 {
+namespace tiles {
+class Helper {
+ private:
+ public:
+  Helper();
+  ~Helper();
+};
+}  // namespace tiles
+}  // namespace v1
+}  // namespace mavix
