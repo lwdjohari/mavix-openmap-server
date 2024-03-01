716b37a88af391ee3eb5df30020e95e6400dc0aa
lwdjohari <linggawasistha.djohari@outlook.com>
2024-03-01 19:55:44 +0800

    Add library mavix-renderer for handling raster renderer operations

---
+++ deps/mavix-renderer/src/mavix/v1/renderer/helper.cc
@@ -0,0 +1,16 @@
+#include "mavix/v1/renderer/helper.h"
+
+#include "helper.h"
+
+namespace mavix {
+namespace v1 {
+
+namespace renderer {
+Helper::Helper() {}
+
+Helper::~Helper() {}
+}  // namespace renderer
+
+}  // namespace v1
+
+}  // namespace mavix
---
+++ deps/mavix-renderer/src/mavix/v1/renderer/helper.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace mavix {
+namespace v1 {
+namespace renderer {
+class Helper {
+ private:
+ public:
+  Helper();
+  ~Helper();
+};
+}  // namespace renderer
+}  // namespace v1
+}  // namespace mavix
