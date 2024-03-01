a3471e61b63204ed50afcde4811d5009f3b38404
lwdjohari <linggawasistha.djohari@outlook.com>
2024-03-01 19:53:03 +0800

    Add library mavix-osm for handling OSM pbf data

---
+++ deps/mavix-osm/src/mavix/v1/osm/helper.cc
@@ -0,0 +1,16 @@
+#include "mavix/v1/osm/helper.h"
+
+#include "helper.h"
+
+namespace mavix {
+namespace v1 {
+
+namespace osm {
+Helper::Helper() {}
+
+Helper::~Helper() {}
+}  // namespace osm
+
+}  // namespace v1
+
+}  // namespace mavix
---
+++ deps/mavix-osm/src/mavix/v1/osm/helper.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace mavix {
+namespace v1 {
+namespace osm {
+class Helper {
+ private:
+ public:
+  Helper();
+  ~Helper();
+};
+}  // namespace osm
+}  // namespace v1
+}  // namespace mavix
