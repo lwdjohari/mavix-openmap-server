0e2ce3c19669db2eca44062ff207751d93e43b8f
lwdjohari <linggawasistha.djohari@outlook.com>
2024-03-01 19:54:17 +0800

    Add library mavix-spatial for handling spatial operations layer

---
+++ deps/mavix-spatial/src/mavix/v1/spatial/helper.cc
@@ -0,0 +1,16 @@
+#include "mavix/v1/spatial/helper.h"
+
+#include "helper.h"
+
+namespace mavix {
+namespace v1 {
+
+namespace spatial {
+Helper::Helper() {}
+
+Helper::~Helper() {}
+}  // namespace spatial
+
+}  // namespace v1
+
+}  // namespace mavix
